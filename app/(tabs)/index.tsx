46c6d86398b9ac7c8cb75440b610cd37043cf492
kledio.myrto <kledio.myrto@3i-solutions.net>
2024-05-02 09:20:21 +0200

    first  commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,33 @@
-import { StyleSheet } from 'react-native';
+import { View } from 'react-native'
+import React, { useMemo, useState } from 'react'
+import ExploreHeader from '@/components/ExploreHeader'
+import { Stack } from 'expo-router';
+import Listings from '@/components/Listings';
+import listingsData from '@/assets/data/airbnb-listings.json'
+import ListingsDataGeo from '@/assets/data/airbnb-listings.geo.json'
+import ListingsMap from '@/components/ListingsMap';
+import ListingsBottomSheet from '@/components/ListingsBottomSheet';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+const Page = () => {
+  const [category, setCategory] = useState('Tiny homes')
+  const items = useMemo(() => listingsData as any, [])
 
-export default function TabOneScreen() {
+  const onDataChanged = (category: string) => {
+    setCategory(category)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View style={{ flex: 1, marginTop: 130 }}>
+      <Stack.Screen
+        options={{
+          header: () => <ExploreHeader onCategoryChanged={onDataChanged} />,
+        }}
+      />
+      {/* <Listings listings={items} category={category} /> */}
+      <ListingsMap listings={ListingsDataGeo} />
+      <ListingsBottomSheet listings={items} category={category} />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+export default Page
---
+++ components/ListingsMap.tsx
@@ -0,0 +1,111 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
+import MapView from 'react-native-map-clustering'
+import { defaultStyles } from '@/constants/Styles'
+import { useRoute } from '@react-navigation/native'
+import { useRouter } from 'expo-router'
+
+interface Props {
+    listings: any
+}
+
+const INITIAL_REGION = {
+    latitude: 41.33,
+    longitude: 19.82,
+    latitudeDelta: 12,
+    longitudeDelta: 12,
+};
+
+const ListingsMap = ({ listings }: Props) => {
+    const router = useRouter()
+
+    const onMarkerSelected = (item: ListingGeo) => {
+        router.push(`/listing/${item.properties.id}`)
+        console.log(item);
+    }
+
+    const renderCluster = (cluster: any) => {
+        const { id, geometry, onPress, properties } = cluster;
+        const points = properties.point_count;
+
+        return (
+            <Marker key={`cluster-${id}`}
+                onPress={onPress}
+                coordinate={{
+                    longitude: geometry.coordinates[0],
+                    latitude: geometry.coordinates[1]
+                }}>
+                <View style={styles.marker}>
+                    <Text
+                        style={{
+                            color: '#000',
+                            textAlign: 'center',
+                            fontFamily: 'mon-sb',
+                        }}>
+                        {points}
+                    </Text>
+                </View>
+            </Marker>
+        )
+    }
+
+    return (
+        <View style={defaultStyles.container}>
+            <MapView
+                animationEnabled={false}
+                style={StyleSheet.absoluteFill}
+                provider={PROVIDER_GOOGLE}
+                showsUserLocation
+                showsMyLocationButton
+                initialRegion={INITIAL_REGION}
+                clusterColor='#fff'
+                clusterTextColor='#000'
+                clusterFontFamily='mon-sb'
+                renderCluster={renderCluster}
+            >
+                {listings.features.map((item: ListingGeo) => (
+                    <Marker
+                        key={item.properties.id}
+                        onPress={() => onMarkerSelected(item)}
+                        coordinate={{
+                            latitude: +item.properties.latitude,
+                            longitude: +item.properties.longitude
+                        }}
+                    >
+                        <View style={styles.marker}>
+                            <Text style={styles.markerText}>€ {item.properties.price}</Text>
+                        </View>
+                    </Marker>
+                ))}
+            </MapView>
+        </View>
+    )
+}
+
+export default ListingsMap
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    marker: {
+        padding: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+        elevation: 5,
+        borderRadius: 12,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowRadius: 6,
+        shadowOffset: {
+            width: 1,
+            height: 10,
+        },
+    },
+    markerText: {
+        fontSize: 14,
+        fontFamily: 'mon-sb',
+    }
+})
